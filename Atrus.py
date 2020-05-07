ff37d07daa614c9628e821862dd04825c5d6da8c
Eric Delmonico <ead1758@rit.edu>
2020-05-07 18:29:15 -0400

    Changed up functionality to be a message-reply bot
    If you message AtrusBot anything at all, it will give to you to the top deals of the week and the top deals of the day, with links to the posts.

--- Atrus.py
+++ Atrus.py
@@ -1,24 +1,55 @@
 import praw
 
 reddit = praw.Reddit('atrusbot')
-sub = reddit.subreddit('NintendoSwitchDeals')
 
-submissions = sub.top(time_filter='week', limit=50)
+def get_deal_titles(submissions):
+    body = ''
+    for submission in submissions:
+        # grab the title for processing
+        title = submission.title
 
-for submission in submissions:
-    # grab the title for processing
-    title = submission.title
+        # make sure the deal is in the USA. This should  
+        # (in theory) chop out any [META] posts as well.
+        if (title.lower().find('usa') == -1 and title.lower().find('us') == -1):
+            continue
 
-    # make sure the deal is in the USA. This should  
-    # (in theory) chop out any [META] posts as well.
-    if (title.lower().find('usa') == -1 and title.lower().find('us') == -1):
+        # really only looking for eshop/amazon/gamestop deals
+        if (title.lower().find('eshop') == -1 and 
+            title.lower().find('amazon') == -1 and
+            title.lower().find('gamestop') == -1):
+            continue
+        
+        # giving the post title and a link to the post
+        body += '[' + submission.title + ']' + '(' + submission.permalink + ')' + '\n\n'
+    return body
+
+def get_weekly_deals():
+    # get the top 50 submisions for the 
+    # week from /r/nintendoswitchdeals
+    sub = reddit.subreddit('NintendoSwitchDeals')
+        
+    # put together a message
+    # -----------------------
+    # weekly deals
+    submissions = sub.top(time_filter='week', limit=50)
+    body = '#Top deals of the week\n\n\n\n'
+    body += get_deal_titles(submissions)
+    
+    #daily deals
+    submissions = sub.top(time_filter='day', limit=30)
+    body += '\n\n\n\n#Top Deals of the day\n\n\n\n'
+    body += get_deal_titles(submissions)
+    # -----------------------
+
+    return body
+
+# if AtrusBot has gotten any messages, 
+# reply back with a list of on sale games
+inbox = reddit.inbox.unread()
+for item in inbox:
+    # do not reply to comments
+    if (item.was_comment == True):
         continue
-
-    # really only looking for eshop/amazon/gamestop deals
-    if (title.lower().find('eshop') == -1 and 
-        title.lower().find('amazon') == -1 and
-        title.lower().find('gamestop') == -1):
-        continue
-
-    print(title)
-
+    item.reply(get_weekly_deals())
+    item.mark_read()
+    
